4c05c0e8ac2416f456a1f6d44f863a45af16232d
Ali Emre SAVAS <aliemresavas.95@gmail.com>
2020-12-29 23:28:40 +0000

    User model transferred to main file

--- pwcase.py
+++ pwcase.py
@@ -3,6 +3,7 @@ from flask.helpers import flash
 from flask_sqlalchemy import SQLAlchemy
 from templates.forms.LoginForm import LoginForm
 from templates.forms.RegisterForm import RegisterForm
+from templates.models.Users import Users
 from passlib.handlers.sha2_crypt import sha256_crypt
 
 app = Flask(__name__)
@@ -10,7 +11,18 @@ app.secret_key = "pwcase"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:asd123@localhost/NEWDATABASENAME' # Buraya yeni databasein adi yazilacak.
 db = SQLAlchemy(app)
 
-# DB modelleri eklenecek!
+# DB Models
+# User table model
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30), unique=True, nullable=False)
+    name_surname = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(80))
+    password = db.Column(db.Text, nullable=False)
+    secret_key = db.Column(db.Integer, nullable=False)
+
+
+
 
 #Index
 @app.route("/")
@@ -28,6 +40,7 @@ def login():
 @app.route("/register", methods=["GET","POST"])
 def register():
     form = RegisterForm(request.form)
+    
     return render_template("pages/register.html", form=form)
 
 
